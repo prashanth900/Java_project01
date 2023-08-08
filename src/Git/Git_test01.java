bd2e69f8b5d71784308e37fadbc1fc42ec58fcd4
prashanth900 <prashanthgoud3998@gmail.com>
2023-08-08 22:59:11 +0545

    inital commit

---
+++ src/Git/Git_test01.java
@@ -0,0 +1,15 @@
+package Git;
+
+public class Git_test01 {
+
+	public static void main(String[] args) {
+		int a = 10;
+        int b = 20;
+        int c = a + b ;
+        
+        
+        
+        System.out.println(c);
+	}
+
+}
